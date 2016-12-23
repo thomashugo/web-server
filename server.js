cbdc5a4e5c2b51c7259cf1039f9825a3d122e913
Thomas Hugo <thomashugo@gmail.com>
2016-12-23 14:17:54 -0800

    Update About Page

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(middleware.logger);
 // app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-  res.send('<h1>About Us</h1>');
+  res.send('<h1>About Us!</h1>');
 });
 
 app.use(express.static(__dirname + '/public'));
